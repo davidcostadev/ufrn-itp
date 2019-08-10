054a5dd1278834807cc7ba188032872e5e764e4c
David Costa <davidcostadev@gmail.com>
2019-08-09 22:47:58 -0300

    Add the first class

---
+++ src/aula-01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Olá\n");
+
+  int value1 = 10;
+  int value2 = 5;
+  int value3 = value1 + value2;
+
+  printf("Digite um número:\n");
+  scanf("%i", &value1);
+
+  printf("v1: %d, v2: %d, v3: %d\n", value1, value2, value3);
+
+  return 0;
+}
