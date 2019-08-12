c6c837ad58a105279c69388f6d875a9203dd0c1d
David Costa <davidcostadev@gmail.com>
2019-08-12 19:28:15 -0300

    Add uri_1011 Challenge

---
+++ src/uri_1011.c
@@ -0,0 +1,18 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1011
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  double radius;
+
+  scanf("%lf", &radius);
+
+  float PI = 3.14159;
+
+  double calc = (4 / 3.0) * PI * pow(radius, 3);
+
+  printf("VOLUME = %.3lf\n", calc);
+
+  return 0;
+}
