350eb612a9703a196657a9d4eb0fd60eaa8e6b94
David Costa <davidcostadev@gmail.com>
2019-08-21 20:46:31 -0300

    Update and finish the exercice 03

--- src/aula_07_exercice_03.c
+++ src/aula_07_exercice_03.c
@@ -6,48 +6,34 @@ int main()
   double x, y;
   char operation;
   int exit = 0;
+  printf("Enter with your expression\nEx.: 1+3 and ENTER.\nYou can use +, -, * and /.\n");
+  scanf("%lf %c %lf", &x, &operation, &y);
 
-  printf("------- Calc Shell -------\n");
-  printf("Press 'q' and enter or CTRL+C to exit any time\n");
-
-  while(exit == 0) {
-
-    
-    printf("> ");
-
-    scanf("%lf %c %lf", &x, &operation, &y);
-
-    switch (operation)
+  switch (operation)
+  {
+  case '+':
+    printf("> %lf\n", x + y);
+    break;
+  case '-':
+    printf("> %lf\n", x - y);
+    break;
+  case '*':
+    printf("> %lf\n", (x * y));
+    break;
+  case '/':
+    if (y == 0)
     {
-    case '+':
-      printf("> %lf\n", x + y);
-      break;
-    case '-':
-      printf("> %lf\n", x - y);
-      break;
-    case '*':
-      printf("> %lf\n", (x * y));
-      break;
-    case '/':
-      if (y == 0) {
-        printf("Error: You can't divide %lg by Zero\n", x);
-      } else {
-        printf("> %lf\n", x / y);
-      }
-      break;
-    case 'q':
-      exit = 1;
-      break;
-
-    default:
-      printf("Error: Operator Invalid, Please use just + ,- , * and / operators\n");
-      exit = 1;
-      break;
+      printf("Error: You can't divide %lg by Zero\n", x);
     }
+    else
+    {
+      printf("%lf\n", x / y);
+    }
+    break;
 
-    x = 0;
-    y = 0;
-    operation = '+';
+  default:
+    printf("Error: Operator Invalid, Please use just + ,- , * and / operators\n");
+    break;
   }
 
   return 0;
